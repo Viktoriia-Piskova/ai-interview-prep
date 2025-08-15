a31902491f14087ec95f35418f390824b2d881cd
piskova <v.piskova@beetroot.se>
2025-08-15 18:48:14 +0300

    feat: add sign in and sign up with Firebase

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -7,10 +7,16 @@ import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import FormField from "@/components/FormField";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -39,12 +45,47 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
+        const { name, email, password } = values;
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result?.success) {
+          console.log(result?.message)
+          toast.error(result?.message);
+          return;
+        }
+
         toast.success("Account created successfully. Please sign in");
         router.push("/sign-in");
       } else {
+        const { email, password } = values;
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if (!idToken) {
+          toast.error("Sign in failed");
+          return;
+        }
+        await signIn({ email, idToken });
+
         toast.success("Signed in successfully");
         router.push("/");
       }
---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,81 @@
+"use server";
+
+import { db, auth } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
+
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
+
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User already exists, please sign in",
+      };
+    }
+
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in",
+    };
+  } catch (e: any) {
+    console.log("Error creating a user", e.message);
+    if (e.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "This email is already taken",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Failed to create an account",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK,
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exist, create an account",
+      };
+    }
+
+    await setSessionCookie(idToken);
+  } catch (e) {
+    console.log(e);
+
+    return {
+      success: false,
+      message: "Failed to log into an account",
+    };
+  }
+}
