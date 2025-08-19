2f6684a72b82735b1eb5f3a76579530929ec3a71
piskova <v.piskova@beetroot.se>
2025-08-19 13:03:39 +0300

    feat: add protected routes and redirects

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -5,6 +5,21 @@ import { cookies } from "next/headers";
 
 const ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
 
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK,
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
 export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
   try {
@@ -42,21 +57,6 @@ export async function signUp(params: SignUpParams) {
   }
 }
 
-export async function setSessionCookie(idToken: string) {
-  const cookieStore = await cookies();
-  const sessionCookie = await auth.createSessionCookie(idToken, {
-    expiresIn: ONE_WEEK,
-  });
-
-  cookieStore.set("session", sessionCookie, {
-    maxAge: ONE_WEEK,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    path: "/",
-    sameSite: "lax",
-  });
-}
-
 export async function signIn(params: SignInParams) {
   const { email, idToken } = params;
   try {
@@ -78,4 +78,30 @@ export async function signIn(params: SignInParams) {
       message: "Failed to log into an account",
     };
   }
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    console.log(userRecord.data());
+    if (!userRecord.exists) return null;
+
+    return { ...userRecord.data(), id: userRecord.id } as User;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user;
 }
